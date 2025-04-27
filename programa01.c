536b9947d1d3a7d93eebd0118babead2b75f0aa0
leosantossirqueira <leo.ss.78k@gmail.com>
2025-04-27 04:02:46 -0300

    Add files via upload
    programa pra teste, para testarmos e baixarmos o executavel, ja que não ta rolando pelo cmd

---
+++ programa01.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+
+int main() {
+ int num1, num2;
+ printf("Digite o primeiro nome");
+ scanf("%d", &num1);
+ printf("Digite o segundo numero");
+ scanf("%d", &num2);
+ int soma=num1+num2;                                                    
+printf("A soma de %d e %d é: %d\n", num1, num2, soma);
+    return 0;
+}                                                                                          
